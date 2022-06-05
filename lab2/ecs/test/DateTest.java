3747cf9ae01782a5a7f8b1c825a6165325c5e08c
assia7R <argui076@uottawa.ca>
2022-06-05 09:41:13 -0400

    Add files via upload

---
+++ lab2/ecs/test/DateTest.java
@@ -0,0 +1,123 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DateTest {
+
+  @Test
+  void testcase_1() {
+    Date d = new Date(1700,6,20);
+    assertEquals(new Date(1700,6,21), d.nextDate());
+  }
+
+  @Test
+  void testcase_2() {
+    Date d = new Date(2005,4,15);
+    assertEquals(new Date(2005,4,16), d.nextDate());
+  }
+
+  @Test
+  void testcase_3() {
+    Date d = new Date(1901,7,20);
+    assertEquals(new Date(1901,7,21), d.nextDate());
+  }
+
+  @Test
+  void testcase_4() {
+    Date d = new Date(3456,3,27);
+    assertEquals(new Date(3456,3,28), d.nextDate());
+  }
+
+  @Test
+  void testcase_5() {
+    Date d = new Date(1500,2,17);
+    assertEquals(new Date(1500,2,18), d.nextDate());
+  }
+
+  @Test
+  void testcase_6() {
+    Date d = new Date(1700,6,29);
+    assertEquals(new Date(1700,6,30), d.nextDate());
+  }
+
+  @Test
+  void testcase_7() {
+    Date d = new Date(1800,11,29);
+    assertEquals(new Date(1800,11,30), d.nextDate());
+  }
+
+  @Test
+  void testcase_8() {
+    Date d = new Date(3453,1,29);
+    assertEquals(new Date(3453,1,30), d.nextDate());
+  }
+
+  @Test
+  void testcase_9() {
+    Date d = new Date(444,2,29);
+    assertEquals(new Date(444,3,1), d.nextDate());
+  }
+
+  @Test
+  void testcase_10() {
+    Date d = new Date(2005,4,30);
+    assertEquals(new Date(2005,5,1), d.nextDate());
+  }
+
+  @Test
+  void testcase_11() {
+    Date d = new Date(3453,1,30);
+    assertEquals(new Date(3453,1,31), d.nextDate());
+  }
+
+  @Test
+  void testcase_12() {
+    Date d = new Date(3456,3,30);
+    assertEquals(new Date(3456,03,31), d.nextDate());
+  }
+
+  @Test
+  void testcase_13() {
+    Date d = new Date(1901,7,31);
+    assertEquals(new Date(1901,8,1), d.nextDate());
+  }
+
+  @Test
+  void testcase_14() {
+    Date d = new Date(3453,1,31);
+    assertEquals(new Date(3453,2,1), d.nextDate());
+  }
+
+  @Test
+  void testcase_15() {
+    Date d = new Date(3456,12,31);
+    assertEquals(new Date(3457,01,01), d.nextDate());
+  }
+
+  @Test
+  void testcase_16() {
+  	assertThrows(IllegalArgumentException.class, () -> new Date(1500,2,31));
+  }
+
+  @Test
+  void testcase_17() {
+    assertThrows(IllegalArgumentException.class, () -> new Date(1500,2,29));
+  }
+
+  @Test
+  void testcase_18() {
+    assertThrows(IllegalArgumentException.class, () -> new Date(-1,10,20));
+  }
+
+  @Test
+  void testcase_19() {
+    assertThrows(IllegalArgumentException.class, () -> new Date(1458,15,12));
+  }
+
+  @Test
+  void testcase_20() {
+    assertThrows(IllegalArgumentException.class, () -> new Date(1975,6,-50));
+  }
+
+}
+}
